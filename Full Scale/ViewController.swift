b5e2e989acc22e6d1bd62f42df411c5744e19e32
greatrandz <randolph.d@flightschedulepro.com>
2019-03-14 06:33:26 +0800

    Initial Commit

---
+++ Full Scale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Full Scale
+//
+//  Created by Randolf Dini-ay on 14/03/2019.
+//  Copyright © 2019 Randolf Dini-ay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
